21771ace5ebbdeee833fc0646b8d1d61cb59fcf6
hdavid <hugo.david.tech@proton.me>
2026-07-25 18:14:21 +0200

    add types/index.ts

---
+++ types/index.ts
@@ -0,0 +1,8 @@
+// types/index.ts
+export interface CompteInfo {
+  numero_compte: string;
+  solde: number;
+  // Ajoute d'autres propriétés si nécessaire
+}
+
+export type infoCopro = CompteInfo[];
