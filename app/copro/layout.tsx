5fbedd7d3b5c32cf45bd9263cdd78b449ff3bfaa
hdavid <hugo.david.tech@proton.me>
2026-07-25 16:27:53 +0200

    add readme

--- app/copro/layout.tsx
+++ app/copro/layout.tsx
@@ -18,7 +18,7 @@ export default function ProtectedLayout({
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Gestion Copro</Link>
+              <Link href={"/"}>CoprOS</Link>
 
               {/* Icône Dashboard */}
               <Link
